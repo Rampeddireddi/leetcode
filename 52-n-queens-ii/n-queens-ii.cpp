2d1b5578608d858c7a30a982fcfc8c0b0edfa518
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-02-19 23:13:58 +0530

    Time: 7 ms (28.02%) | Memory: 14.4 MB (8.68%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+int c=0;
+void Ram(int n,int col,vector<string>v,vector<int>&leftrow,vector<int>&lowdiag,vector<int>&updiag){
+    if(col>=n){
+       c+=1;
+        return;
+    }
+    for(int row=0;row<n;row++){
+        if(leftrow[row]==0 && lowdiag[row+col]==0 && updiag[n-1+col-row]==0){
+            v[row][col]='Q';
+            leftrow[row]=1;
+            lowdiag[row+col]=1;
+            updiag[n-1+col-row]=1;
+            Ram(n,col+1,v,leftrow,lowdiag,updiag);
+            v[row][col]='.';
+            leftrow[row]=0;
+            lowdiag[row+col]=0;
+            updiag[n-1+col-row]=0;
+        }
+    }
+}
+
+    int totalNQueens(int n) {
+       vector<string>v(n);
+       string s(n,'.');
+       for(int i=0;i<n;i++){
+           v[i]=s;
+       }
+       vector<int>leftrow(n,0);
+       vector<int>lowdiag(2*n-1,0);
+       vector<int>updiag(2*n-1,0);
+       Ram(n,0,v,leftrow,lowdiag,updiag);
+        return c;
+    }
+};
