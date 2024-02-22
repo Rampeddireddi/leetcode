23d95e8cff404dc02fdc22778a2d0762a1afd831
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-02-22 18:22:56 +0530

    Time: 118 ms (88.83%) | Memory: 65.3 MB (51.77%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -4,26 +4,21 @@ public:
         set<int>s1;
         // set<int>s2;
       for(int i=0;i<trust.size();i++){
-          for(int j=0;j<trust[i].size();j++){
-              if(j==0)s1.insert(trust[i][j]);
+              s1.insert(trust[i][0]);
             //   else s2.insert(trust[i][j]);
-          }
       }
       int t=n*(n+1)/2;
       int sum=0;
-      int s3=0;
+    //   int s3=0;
       for(auto i: s1)sum+=i;
     //   for(auto i:s2)s3+=i;
       int c=0;
       int val=t-sum;
       if(sum==t)return -1;
-
       else{
         for(int i=0;i<trust.size();i++){
-        
               if(trust[i][1]==val)
-              c++;
-          
+              c++;   
       }   
       } 
       if(c==n-1)return val;
