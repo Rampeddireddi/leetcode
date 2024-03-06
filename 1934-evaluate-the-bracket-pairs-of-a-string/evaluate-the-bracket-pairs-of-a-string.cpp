d508d01d20f09b1ae7c791fbcd40928d116ad0d8
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-03-06 17:47:11 +0530

    Time: 300 ms (57.92%) | Memory: 142.8 MB (9.95%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -3,7 +3,7 @@ public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
         string res="";
         int i=0;
-        map<string,string>mp;
+        unordered_map<string,string>mp;
         for(auto i:knowledge)mp[i[0]]=i[1];
         while(i<s.size()){
         string sr="";
@@ -14,7 +14,7 @@ public:
                  sr+=s[i];
                  i++;
              }
-             i++;
+             
              if(mp.find(sr)!=mp.end())
              {
                  res+=mp[sr];
@@ -23,8 +23,9 @@ public:
          }
          else{
              res+=s[i];
-             i++;
+            
          }
+         i++;
         }
         return res;
     }
