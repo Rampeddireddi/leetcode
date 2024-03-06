adfba3563e314e07e2b98cde2bc8b27714d98ec7
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-03-06 17:43:03 +0530

    Time: 456 ms (6.79%) | Memory: 140.9 MB (20.81%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        string res="";
+        int i=0;
+        map<string,string>mp;
+        for(auto i:knowledge)mp[i[0]]=i[1];
+        while(i<s.size()){
+        string sr="";
+         if(s[i]=='('){
+             i++;
+             while(s[i]!=')')
+             {
+                 sr+=s[i];
+                 i++;
+             }
+             i++;
+             if(mp.find(sr)!=mp.end())
+             {
+                 res+=mp[sr];
+             }
+             else res+='?';
+         }
+         else{
+             res+=s[i];
+             i++;
+         }
+        }
+        return res;
+    }
+};
