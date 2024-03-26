f64ba2232664909e72d03038ffeb8f0cbeb14184
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-03-26 21:06:42 +0530

    Time: 80 ms (15.16%) | Memory: 60.6 MB (11.54%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -12,16 +12,18 @@ public:
         //     }
         // }
         // return m1+1;
+          if(find(nums.begin(),nums.end(),1)==nums.end())return 1;
         unordered_set<int>s;
         for(int i=0;i<nums.size();i++){
             s.insert(nums[i]);
         }
         // sort(nums.begin(),nums.end());
+        
         int x=*min_element(nums.begin(),nums.end());
         if(x>1)return 1;
         int m1=*max_element(nums.begin(),nums.end());
         if(m1<1)return 1;
-        if(find(nums.begin(),nums.end(),1)==nums.end())return 1;
+      
         for(int i=1;i<m1;i++)
         {
            if(s.find(i)==s.end())return i;
