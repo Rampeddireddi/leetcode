d0b73ff1bc046198a08204cfe08b2ff9030a39a1
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-03-29 22:08:57 +0530

    Time: 131 ms (62.02%) | Memory: 119.9 MB (84.30%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,19 +1,43 @@
+// class Solution {
+// public:
+//     long long countSubarrays(vector<int>& nums, int k) {
+//         int i=0,j=0;
+//         int maxE=*max_element(nums.begin(),nums.end());
+//         long long ans=0;
+//         int maxCount=0;
+//         while(j<nums.size()){
+//             if(nums[j]==maxE) maxCount++;
+//             while(maxCount>=k){
+//                 ans+=nums.size()-j;
+//                 if(nums[i]==maxE) maxCount--;
+//                 i++;
+//             }
+//             j++;
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
-        int i=0,j=0;
-        int maxE=*max_element(nums.begin(),nums.end());
-        long long ans=0;
-        int maxCount=0;
-        while(j<nums.size()){
-            if(nums[j]==maxE) maxCount++;
-            while(maxCount>=k){
-                ans+=nums.size()-j;
-                if(nums[i]==maxE) maxCount--;
-                i++;
+        int l=0,len=nums.size();
+        long long cnt=0;
+        int m=*max_element(nums.begin(),nums.end());
+        int r=0;
+   
+        for(int i=0;i<len;i++){
+            if(nums[i]==m)r++;
+
+            if(r>=k){
+             
+                while(r>=k){
+                    if(nums[l]==m)r--;
+                    l++;
+                    cnt+=len-i;
+                }
+               
             }
-            j++;
         }
-        return ans;
+        return cnt;
     }
 };
