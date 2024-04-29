d88178f6461cf8651c537ce3cdb1c30a771eb9f9
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-04-29 18:32:35 +0530

    Time: 125 ms (29.32%) | Memory: 91.4 MB (25.13%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
-       int minOperations(vector<int>& A, int k) {
-        for (int a : A)
-            k ^= a;
-        return __builtin_popcount(k);
+    int minOperations(vector<int>& v, int k) {
+        int n=v.size();
+        int ans=0;
+        for(int i=0;i<=30;i++) {
+            int b=0;
+            for(int j=0;j<n;j++) {
+                if(((v[j]>>i)&1)==1)
+                    b=b^1;
+            }
+            if(((k>>i)&1)!=b)
+                ans++;
+        }
+        return ans;
     }
 };
