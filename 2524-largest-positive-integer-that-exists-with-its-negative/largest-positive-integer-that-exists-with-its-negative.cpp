e0cd23f4cbaf2ad7fbf318bce76435a4b596488c
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-05-02 19:55:30 +0530

    Time: 12 ms (87.94%) | Memory: 22.9 MB (52.66%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        map<int,int>mp;
+        // map<int,int>mp;
        sort(nums.begin(),nums.end(),greater<int>());
-       for(int i=0;i<nums.size();i++){
-        mp[nums[i]]++;
-       } 
-       for(int i=0;i<nums.size();i++){
+    //    for(int i=0;i<nums.size();i++){
+    //     mp[nums[i]]++;
+    //    } 
+    //    for(int i=0;i<nums.size();i++){
+    //     int res=-nums[i];
+    //     if(mp.find(res)!=mp.end())return abs(res);
+    //    }
+    //    return -1;
+    if(nums[0]<0)return -1;
+    int i=0,j=nums.size()-1;
+    while(i<j){
         int res=-nums[i];
-        if(mp.find(res)!=mp.end())return abs(res);
-       }
-       return -1;
+        if(res==nums[j])return nums[i];
+        else if(nums[j]<res){
+            while(nums[j]<res)j--;
+        }
+        else if(nums[j]>res){
+            i++;
+        }
+    }
+    return -1;
     }
 };
