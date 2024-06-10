55bc29a0a08824f4d46f700372faafd032037acf
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-06-10 19:49:32 +0530

    Time: 5 ms (31.17%) | Memory: 10.2 MB (44.04%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int>v;
+        vector<int>v=heights;
         int cnt=0;
-        for(int i=0;i<heights.size();i++){
-            v.push_back(heights[i]);
-        }
+       
         sort(v.begin(),v.end());
         for(int i=0;i<heights.size();i++){
             if(v[i]!=heights[i])cnt++;
