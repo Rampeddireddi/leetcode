b8660a360b39a23b56cd887d66e37c3a2280641b
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-06-18 19:22:03 +0530

    Time: 67 ms (35.31%) | Memory: 44.2 MB (25.52%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-    vector<pair<int,int>> jobs;
-    int n=profit.size(),it=0,res=0,fr=0;
-    for(int i=0;i<n;i++){
-        jobs.push_back({difficulty[i],profit[i]});
-    }
-    sort(jobs.begin(),jobs.end());
-    sort(worker.begin(),worker.end());
-    for(int & ability : worker){
-        while(it<n && ability>=jobs[it].first){
-            res=max(res,jobs[it].second);
-            it++;
-        }
-    fr+=res;
-    }
-    return fr;
+       unordered_map<int,int>mp;
+       int n=profit.size(),j=0,best=0,res=0;
+    
+       for(int i=0;i<profit.size();i++){
+           if(mp.find(difficulty[i])!=mp.end()){
+            mp[difficulty[i]]=max(mp[difficulty[i]],profit[i]);
+        
+           }
+           else mp[difficulty[i]]=profit[i];  // This might overwrite profits for the same difficulty
+       } 
+       sort(difficulty.begin(), difficulty.end());  // Sorting difficulty array after populating the map
+       sort(worker.begin(), worker.end());
+       for(int i=0;i<worker.size();i++){
+           while(j<n && worker[i]>=difficulty[j] ){  // Incorrect indexing with sorted difficulty array
+               best=max(best,mp[difficulty[j]]);
+               j++;
+           }
+           res+=best;
+       }
+       return res;
     }
 };
