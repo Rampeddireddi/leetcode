43e353c689fae4d5f969a568ec7d1f31457cae0c
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-08-04 22:46:55 +0530

    Time: 100 ms (22.70%) | Memory: 27.2 MB (39.10%) - LeetSync

---
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        long long sum=0;
+        vector<int>res;
+      for(int i=0;i<nums.size();i++){
+        sum=0;
+        for(int j=i;j<nums.size();j++){
+            sum+=nums[j];
+        res.push_back(sum);      
+        }
+      }
+      long long newsum=0;
+      sort(res.begin(),res.end());
+      for(int i=left-1;i<right;i++){
+        newsum+=res[i];
+      }  
+     long long mod=pow(10,9)+7;
+      return newsum%mod;
+    }
+};
