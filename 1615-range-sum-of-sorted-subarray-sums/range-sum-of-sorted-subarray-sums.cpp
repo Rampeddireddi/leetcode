77250e489def981f1ce094567c9765c618790da2
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-08-04 22:47:37 +0530

    Time: 99 ms (24.49%) | Memory: 27.2 MB (31.24%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
         long long sum=0;
         vector<int>res;
       for(int i=0;i<nums.size();i++){
