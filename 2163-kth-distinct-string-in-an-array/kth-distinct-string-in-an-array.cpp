c96bf4885ffd31bf0e4ded5e5aaa58c6912eb3e5
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-08-05 22:11:22 +0530

    Time: 20 ms (36.87%) | Memory: 18.4 MB (31.70%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
         map<string,int>mp;
         for(int i=0;i<arr.size();i++){
             mp[arr[i]]++;
