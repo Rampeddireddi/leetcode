6b1fd95f3d08b1515d9a42dd5f9574846344c5c8
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-10-15 18:30:34 +0530

    Time: 9 ms (100.00%) | Memory: 15.7 MB (14.11%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
         long long minswaps=0;
         long long black=0;
         for(int i=0;i<s.size();i++){
