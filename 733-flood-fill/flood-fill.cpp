5237a36d88b936295be8bdeb52499df2052cda7a
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2025-01-27 23:19:18 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (17.56%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        queue<pair<int,int>>q;
+        int n=image.size();
+        int m=image[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int check=image[sr][sc];
+        image[sr][sc]=color;
+        q.push({sr,sc});
+        vis[sr][sc]=2;
+        int rows[4]={0,0,1,-1};
+        int cols[4]={1,-1,0,0};
+        while(!q.empty()){
+            auto node=q.front();
+            int r=node.first;
+            int c=node.second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nr=r+rows[i];
+                int nc=c+cols[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0 && image[nr][nc]==check){
+                    q.push({nr,nc});
+                    image[nr][nc]=color;
+                    vis[nr][nc]=2;
+                }
+            }
+        }
+        return image;
+    }
+};
