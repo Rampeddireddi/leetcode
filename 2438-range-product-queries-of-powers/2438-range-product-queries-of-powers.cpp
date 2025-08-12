b9727b56e1859a4757e0037be455d97445e122cc
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2025-08-12 18:36:10 +0530

    Time: 23 ms (55.61%), Space: 147.5 MB (77.01%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
@@ -1,39 +1,24 @@
+int MOD = 1e9 + 7;
+typedef long long ll;
 class Solution {
 public:
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
-  int mod = 1e9+7;
-      vector<int>power;
-      //power vector generation
-    int base=0;
-      while(n){
-        if(n&1){
-            power.push_back(1<<base);
-        }
+        vector<ll> powers;
+        int base=0;
+       while(n){
+        if(n&1)powers.push_back(1<<base);
         base++;
-        n=n>>1;
-      }
-//   for(auto i:power)cout<<i<<" ";
-
-      vector<long long>prefix(power.size(),0);
-      long long pre=1;
-      for(int i=0;i<power.size();i++){
-
-        pre=(pre*power[i]);
-
-        prefix[i]=pre;
-      }
-      for(auto i:prefix)cout<<i<<" ";
-      vector<int>res;
-      int i1,i2;
-      for(int i=0;i<queries.size();i++){
-        i1=queries[i][0];
-        i2=queries[i][1];
-        if(i1-1 >=0){
-            res.push_back((prefix[i2]/prefix[i1-1])%mod);
+        n>>=1;
+       }
+        vector<int> ans;
+        for(auto &it:queries) {
+            ll temp=1;
+            for(int i=it[0];i<=it[1];i++) {
+                temp*=powers[i];
+                temp%=MOD;
+            }
+            ans.push_back(temp);
         }
-        else res.push_back((prefix[i2])%mod);
-      }
-      return res;
-
+        return ans;
     }
 };
