f20154a75beba038bb7f55b6c403ee66ad6f0b3d
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2025-08-14 12:20:20 +0530

    Time: 6 ms (71.47%), Space: 15.3 MB (52.28%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int posNegways(int ind, int target, vector<int>&nums,map<pair<int,int>,int>&dp){
-        if(ind<0){
-            if(target==0)return 1;
-            // if(target==0 && nums[ind]==0)return 2;
-            return 0;
-        }
-        if(dp.find({ind,target})!=dp.end())return dp[{ind,target}];
-        int neg=posNegways(ind-1,target-nums[ind],nums,dp);
-        int pos=posNegways(ind -1,target+nums[ind],nums,dp);
-        return dp[{ind,target}]=neg+pos;
+    int heybro(int ind,int tar, vector<int>&nums, vector<vector<int>>&dp){
+           if (ind == 0) {
+        if (tar == 0 && nums[0] == 0)
+            return 2; 
+        if (tar == 0 || tar == nums[0])
+            return 1;
+        return 0; 
+    };
+            if(dp[ind][tar]!=-1)return dp[ind][tar];
+           int embabu_laddu_vadha=heybro(ind-1,tar,nums,dp);
+           int laddu_kavala=0;
+           if(nums[ind]<=tar){
+            laddu_kavala=heybro(ind-1, tar-nums[ind],nums,dp);
+           }
+           return dp[ind][tar]=(embabu_laddu_vadha + laddu_kavala);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-       map<pair<int,int>,int>dp;
-       return posNegways(nums.size()-1,target,nums,dp);
+      int n=nums.size();
+        int sum=0,cnt=0;
+        for(auto &i: nums)sum+=i;
+        // cout<<sum;
+        if(target>sum)return 0;
+        if((sum-target) % 2 !=0 || sum-target<0)return 0;
+        int tar=(sum-target)/2;
 
-        //   return cnt;
+      vector<vector<int>>dp(n,vector<int>(tar+1,-1));
+    return  heybro(n-1,tar,nums,dp);
+    
+    //   return cnt;
     }
 };
