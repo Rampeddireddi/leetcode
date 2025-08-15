5c808f061a87e98b718ea7cb925feaf54d6a75ec
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2025-08-15 19:32:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (18.14%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -3,11 +3,7 @@ public:
     bool isPowerOfFour(int n) {
         if(n<=0)return false;
         if((n&(n-1)))return false;
-        int cnt=0;
-        while(n){
-            cnt++;
-            n=n>>1;
-        }
+        int cnt=floor(log2(n))+1;
         return cnt&1;
     }
 };
